01312dc15ebe31c2c5083e94ad979b2db8ea63ca
smoot <smoothtacomann@gmail.com>
2018-10-06 10:47:28 -0400

    added psuedocode to js

---
+++ javascripts/main.js
@@ -0,0 +1,22 @@
+// add fire base
+//button for adding hobbies
+//grab user hobby
+//temp object for holding employee
+//upload hobby data to firebase
+//console log to console to make sure its all working
+//clear text boxes
+
+
+
+//firebase even for adding employee to database and a row in our html when user adds hobbies
+
+//date hobby was started
+
+//
+
+
+
+
+// add weather api
+// add _ api
+
