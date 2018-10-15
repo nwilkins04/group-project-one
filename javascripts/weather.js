a5c64f74db458970c91c3d948b18b40d9c257884
Max Rosen <maxcrosen@gmail.com>
2018-10-15 18:51:22 -0400

    this has both of the working api

--- javascripts/weather.js
+++ javascripts/weather.js
@@ -1,10 +1,14 @@
-// console.log ("weather js linked")
-
-// //ajax for button called
-// $.ajax ({
-//     url: "https://openweathermap.org/current" + "&api_key=2286d2850daceff12f84b544e1d347c2";
-//     method: "GET"
-// }).then(function(response) {
-//     var results = response.data;
-//     $("#weather-info")
-// });
+settings = {
+    "async": true,
+    "crossDomain": true,
+    "url": "https://api.darksky.net/forecast/3acbfa7cea4a91d37edd64fe44ab7aaa/" + lat + "," + lon,
+    "method": "GET",
+    "headers": {
+      'Access-Control-Allow-Origin': '*',
+      "Cache-Control": "no-cache",
+      "Postman-Token": "fd729064-e7a4-4955-b898-a70bdad3fa57"
+    }
+  }
+  $.ajax(settings).done(function (response) {
+    console.log(response);
+  });
